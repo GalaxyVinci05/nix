bbf53d95a87e1c9bb74f98a7b8015ba15ab94a48
GalaxyVinci05 <vincenzopaolo0@gmail.com>
2025-01-21 20:56:47 +0100

    removed blender for asus (now on flatpak)

--- hosts/asus/packages.nix
+++ hosts/asus/packages.nix
@@ -4,7 +4,6 @@
   # List asus packages installed in system profile.
   environment.systemPackages = with pkgs; [
     audacity
-    blender
     davinci-resolve
     kdePackages.kdenlive
     nvtopPackages.nvidia
