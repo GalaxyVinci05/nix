7e984b42622e862327e13384dde53348107e799a
GalaxyVinci05 <vincenzopaolo0@gmail.com>
2025-07-20 15:14:05 +0200

    enabled musnix realtime kernel

--- modules/studio/studio.nix
+++ modules/studio/studio.nix
@@ -1,6 +1,10 @@
 {
   services.pipewire.jack.enable = true;
   security.rtkit.enable = true;
+  
   # Real-time audio
-  musnix.enable = true;
+  musnix = {
+    enable = true;
+    kernel.realtime = true;
+  };
 }
